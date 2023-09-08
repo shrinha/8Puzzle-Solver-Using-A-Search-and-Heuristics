24d6a5e4bb3ec23866203a0d480a456a64c3dfea
shrinha <95378303+shrinha@users.noreply.github.com>
2023-09-08 19:24:07 +0530

    Update astar_utils.py
    Reverted to Old Code

--- astar_utils.py
+++ astar_utils.py
@@ -1,11 +1,14 @@
-import streamlit as st
+from queue import PriorityQueue
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import seaborn as sns
 from matplotlib.colors import ListedColormap
 
-target = ((1, 2, 3), (4, 5, 6), (7, 8, 0))
+target = ((1, 2, 3),
+          (4, 5, 6),
+          (7, 8, 0))
 
 def find_blank(grid):
     for i in range(3):
@@ -13,10 +16,160 @@ def find_blank(grid):
             if grid[i][j] == 0:
                 return i, j
 
-def get_plots(output, cmap_name='coolwarm'):
+def get_children(grid):
+    children = []
+    bx, by = find_blank(grid)
+    if bx < 2:
+        temp = [list(i) for i in grid]
+        temp[bx][by], temp[bx + 1][by] = temp[bx + 1][by], temp[bx][by]
+        t = tuple(tuple(i) for i in temp)
+        children.append(t)
+
+    if bx > 0:
+        temp = [list(i) for i in grid]
+        temp[bx][by], temp[bx - 1][by] = temp[bx - 1][by], temp[bx][by]
+        t = tuple(tuple(i) for i in temp)
+        children.append(t)
+
+    if by < 2:
+        temp = [list(i) for i in grid]
+        temp[bx][by], temp[bx][by + 1] = temp[bx][by + 1], temp[bx][by]
+        t = tuple(tuple(i) for i in temp)
+        children.append(t)
+
+    if by > 0:
+        temp = [list(i) for i in grid]
+        temp[bx][by], temp[bx][by - 1] = temp[bx][by - 1], temp[bx][by]
+        t = tuple(tuple(i) for i in temp)
+        children.append(t)
+
+    return children
+
+def heuristic_cost(grid, h_i):
+    if h_i == 1:  # Uniform Cost Search
+        return 0
+
+    if h_i == 2:  # Number of Displaced Tiles
+        cost = 0
+        for i in range(3):
+            for j in range(3):
+                if grid[i][j] == 0:
+                    continue
+                elif grid[i][j] == target[i][j]:
+                    continue
+                else:
+                    cost += 1
+        return cost
+
+    if h_i == 3:  # Manhattan Distance
+        cost = 0
+        for i in range(3):
+            for j in range(3):
+                if grid[i][j] == 0:
+                    continue
+                x = int((grid[i][j] - 1) / 3)
+                y = (grid[i][j] - 1) % 3
+                cost += (abs(x - i) + abs(y - j))
+        return cost
+
+    if h_i == 4:  # Euclidean Distance
+        cost = 0
+        for i in range(3):
+            for j in range(3):
+                if grid[i][j] == 0:
+                    continue
+                x = int((grid[i][j] - 1) / 3)
+                y = (grid[i][j] - 1) % 3
+                cost += np.sqrt((x - i) ** 2 + (y - j) ** 2)
+        return cost
+
+def solve_puzzle(grid, h_i):
+    # if h_i == 1:
+    #     print("Using h1: h(n) = 0")
+
+    # elif h_i == 2:
+    #     print("Using h2: h(n) = Number of Tiles Displaced")
+
+    # elif h_i == 3:
+    #     print("Using h3: h(n) = Manhattan Cost")
+
+    # elif h_i == 4:
+    #     print("Using h4: h(n) = Euclidean Cost")
+
+    bx, by = find_blank(grid)
+    open_list = PriorityQueue()
+    open_list.put([heuristic_cost(grid, h_i), grid])
+    dist = {grid: 0}
+    parent = {grid: grid}
+    closed_list = set()
+    while open_list:
+        prior, curr_state = open_list.get()
+        if curr_state == target:
+            print("Target State reached.")
+            print("Total states in optimal path ", dist[target] + 1)
+            print("Total number of states explored is ", len(closed_list))
+
+            path = []
+            temp = target
+            while temp != grid:
+                path.insert(0, temp)
+                temp = parent[temp]
+            path.insert(0, temp)
+            for i in path:
+                if i == target:
+                    continue
+
+            output = {"states_explored": len(closed_list),
+                      "optimal_path": path}
+            return output
+
+        bx, by = find_blank(curr_state)
+        children = get_children(curr_state)
+        for i in children:
+            i = tuple(tuple(j) for j in i)
+            if i not in closed_list and (i not in dist or dist[i] > dist[curr_state] + 1):
+                dist[i] = dist[curr_state] + 1
+                parent[i] = curr_state
+                open_list.put([heuristic_cost(i, h_i) + dist[i], i])
+
+            closed_list.add(curr_state)
+
+    # print("Target State not found.")
+    # print("Number of States visited is ", len(closed_list))
+    # return
+    
+def get_inversions(arr):
+    inv_count = 0
+    empty_value = -1
+    for i in range(0, 8):
+        for j in range(i + 1, 8):
+            if arr[j] != empty_value and arr[i] != empty_value and arr[i] > arr[j]:
+                inv_count += 1
+    return inv_count
+
+def soln_exists(grid) :
+    # Check input validity
+    input_list = [j for sub in grid for j in sub]
+    
+    for i in range(9):
+        if i not in input_list:    
+            return False
+    
+    input_list.remove(0)
+    
+    # Count inversions in given 8 puzzle
+    inv_count = get_inversions(input_list)
+
+    # Can't be solved if inversions is odd
+    return (inv_count % 2 == 0)
+
+
+def get_plots(output):
     paths = output['optimal_path']
+        
+    # plt.rcParams["animation.html"] = "jshtml"
     plt.rcParams["figure.figsize"] = (3, 2)
-    plt.rcParams['figure.dpi'] = 150
+    plt.rcParams['figure.dpi'] = 150  
     plt.ioff()
 
     def plot(i):
@@ -24,45 +177,19 @@ def get_plots(output, cmap_name='coolwarm'):
         ax = fig.add_subplot(111)
         my_cmap = ListedColormap(['bisque']).copy()
         my_cmap.set_under('greenyellow')
-        sns.heatmap(
-            paths[i], annot=True, annot_kws={"size": 10}, cbar=False,
-            linewidths=3, linecolor='black', cmap=cmap_name,
-            yticklabels=False, xticklabels=False, vmin=1, ax=ax
-        )
-
-        title = str(i+1) + ' out of ' + str(len(paths)) + ' items in the optimal path'
+        sns.heatmap(paths[i], annot = True, annot_kws={"size":10}, cbar = False, linewidths= 3,
+                    linecolor='black', cmap= my_cmap, yticklabels=False, xticklabels=False, vmin = 1, ax = ax)
+        
+        title = str(i+1) + ' out of ' + str(len(paths)) + ' items in optimal path'
         plt.title(title)
         plt.tight_layout()
-
+        
     n_frames = len(paths)
     fig, ax = plt.subplots()
 
-    anim = animation.FuncAnimation(fig, plot, frames=n_frames, interval=1000)
-
+    anim = animation.FuncAnimation(fig, plot, frames = n_frames, interval = 1000)
+    
     return anim
 
-# Streamlit UI
-st.title("8-Puzzle Solver with Streamlit")
 
-st.sidebar.header("Select Puzzle Configuration")
-
-# Create a 3x3 grid for user input
-input_grid = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-
-for i in range(3):
-    for j in range(3):
-        input_grid[i][j] = st.sidebar.number_input(f"Tile ({i+1}, {j+1})", 0, 8, 0, key=f"{i}{j}")
-
-if st.sidebar.button("Solve Puzzle"):
-    # Check if the input puzzle is solvable
-    input_grid = tuple(tuple(row) for row in input_grid)
-    if not soln_exists(input_grid):
-        st.error("The given puzzle configuration is not solvable.")
-    else:
-        st.info("Solving the puzzle...")
-        output = solve_puzzle(input_grid, h_i=3)
-        cmap_name = st.selectbox("Select Grid Color", ["coolwarm", "viridis", "magma", "inferno"])
-        anim = get_plots(output, cmap_name=cmap_name)
-        st.pyplot(anim.to_jshtml())
-
-st.sidebar.text("Note: Enter values from 0 to 8 (0 represents the blank space).")
+        # sns.heatmap(paths[i], mask = paths[i] > 0, cmap='Blues', annot=False, yticklabels=False, xticklabels=False, ax=ax)
